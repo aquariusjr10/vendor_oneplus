6663769a8e75bd10b1a247dd4a33492fb98dbcd5
aquariusjr10 <deepakjeganathan@gmail.com>
2021-08-22 07:03:19 +0100

    Add MicroG Packages

---
+++ microg/config.mk
@@ -0,0 +1,21 @@
+# MicroG Services
+PRODUCT_PACKAGES += \
+    additional_repos \
+    AuroraStore \
+    Phonesky \
+    com.google.android.maps.xml \
+    com.google.android.maps.jar \
+    FakeStore \
+    FDroid \
+    FDroidPrivilegedExtension \
+    GmsCore \
+    GsfProxy \
+    MozillaNlpBackend \
+    eSpeakTTS \
+    privapp-permissions-auroraservices.xml \
+    AuroraServices \
+    NominatimNlpBackend \
+    bromite-webview
+
+#Bromite 
+LOCAL_MODULE_TARGET_ARCH := arm64
